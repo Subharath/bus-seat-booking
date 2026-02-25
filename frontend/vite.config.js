668ba421cf67e6a9edc9297eda6e3fedfc769179
Chamuditha Subharath <subharathc@gmail.com>
2026-02-25 15:18:28 +0530

    Vite config changed and commented proxy configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
+
+  /*server: {
     port: 3000,
     proxy: {
       '/api': {
@@ -12,5 +13,6 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
-  },
+  },*/
+  
 })
